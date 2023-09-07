5275bfd7e83a55ac8dba370ab22b7a8f62ad8820
Adrien Belminksy <belminksy@gmail.com>
2023-09-07 23:10:15 +0200

    feat: add swagger support

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@
 import { Logger } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { AppModule } from './app.module'
 
 const bootstrap = async () => {
@@ -14,6 +15,12 @@ const bootstrap = async () => {
   app.enableCors()
 
   const logger = new Logger('Bootstrap')
+  const documentConfig = new DocumentBuilder()
+    .setTitle('Overnight API')
+    .setDescription('The API that runs the Overnight platform')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build()
 
   const appPort = configService.get<number>('APP_PORT') ?? 9000
   const environment = configService.get<string>('NODE_ENV')
@@ -21,6 +28,11 @@ const bootstrap = async () => {
   if (environment === undefined) {
     logger.error('The environment variable `NODE_ENV` is not defined')
     return app.close()
+  }
+
+  if (configService.get<string>('SWAGGER_DOC_VISIBILITY') === 'true') {
+    const swaggerDocument = SwaggerModule.createDocument(app, documentConfig)
+    SwaggerModule.setup('/swagger', app, swaggerDocument)
   }
 
   await app.listen(appPort)
